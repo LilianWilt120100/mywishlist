81c6e5422fab14445fc1303b0b5bf8c05c2b91ea
Erwan Bourlon <erwan.bourlon8@etu.univ-lorraine.fr>
2021-01-20 10:56:32 +0100

    suppression de méthodes doublons

--- src/controls/MonControleur.php
+++ src/controls/MonControleur.php
@@ -7,7 +7,6 @@ use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
 
 use \mywishlist\vues\VueWish;
-use \mywishlist\models\Item;
 
 class MonControleur {
 	private $container;
@@ -17,13 +16,6 @@ class MonControleur {
 	}
 	public function accueil(Request $rq, Response $rs, $args) : Response {
 		$rs->getBody()->write('accueil racine du site') ;
-		return $rs;
-	}
-	public function afficherItem(Request $rq, Response $rs, $args) : Response {
-	    // pour afficher un item
-		$item = Item::find( $args['id'] ) ;
-		$vue = new VueWish( [ $item->toArray() ] , $this->container ) ;
-		$rs->getBody()->write( $vue->render( 3 ) ) ;
 		return $rs;
 	}
 
